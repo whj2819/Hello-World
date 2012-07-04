5de6b6b826a4ff4a5ff6f277ea7b63dfbd969fef
whj2819 <whj2819@gmail.com>
2012-07-04 20:58:01 +0800

    add ignore file.

--- perl/chp1/prob02.pl
+++ perl/chp1/prob02.pl
@@ -1,12 +1,19 @@
 #!/usr/bin/perl
 
+
+$dir = shift(@ARGV) || "/tmp";
+print @ARGV ;
+print "\n$dir \n";
+
 =pon
+$foo = $bar || "Default value.";
+print "$foo \n";
 $b = 0;
 $c = 5;
 $a = $b || $c;
 print "$a \n";
-=cut
 
-$x = 0;
+$x = 1;
 $x ||= "test";
 print "__ $x \n";
+=cut
