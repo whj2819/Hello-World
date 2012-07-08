23ac7a34a3ab0221da53b4f921e8b8b10edf94ed
whj2819 <whj2819@gmail.com>
2012-07-08 20:12:33 +0800

    expand variables.

--- perl/chp1/prob11.pl
+++ perl/chp1/prob11.pl
@@ -1,3 +1,23 @@
 #!/usr/bin/perl
 
+use Text::Tabs;
 
+#$tabstop = 4;
+
+while (<>) {
+    print unexpand $_,"\n";
+}
+
+=pon
+$tabstop = 4;
+@line_with_tabs = "This		is 	a test";
+@line_without_tabs ="alextest";
+@expanded_lines = expand(@line_with_tabs);
+@tabulated_lines = unexpand(@line_without_tabs);
+
+print "__ expanded lines @expanded_lines \n";
+print "-- unexpanded lines @tabulated_lines \n";
+
+print @expanded_lines,"\n";
+print @tabulated_lines,"\n";
+=cut
