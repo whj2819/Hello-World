eb28f05ee032de2119e78d54d16322ad185bcb32
whj2819 <whj2819@gmail.com>
2012-07-15 15:05:45 +0800

    nouse

--- unix/chp8/demo4.c
+++ unix/chp8/demo4.c
@@ -1,6 +1,24 @@
 #include "apue.h"
 #include <sys/wait.h>
 
+
+void pr_exit(int status)
+{
+    if ( WIFEXITED(status) ) {
+        printf( "normal termination,exit status = %d \n",WEXITSTATUS(status) );
+    } else if ( WIFSIGNALED(status) ) {
+        printf( "abnormal termination,signal number = %d%s \n",WTERMSIG(status),
+#ifdef WCOREDUMP
+                WCOREDUMP(status) ? "(core file generated)" : "" );
+#else
+        "");
+#endif
+    } else if ( WIFSTOPPED(status) ) {
+        printf("child stopped,signal number = %d \n", WSTOPSIG(status) );
+    }
+}
+
+
 int main(void)
 {
     pid_t   pid;
@@ -12,7 +30,7 @@ int main(void)
         exit(7);
     }
 
-    if ( wait(&status) ) {
+    if ( wait(&status) != pid ) {
         err_sys("wait error");
     }
     pr_exit(status);
@@ -24,7 +42,7 @@ int main(void)
         abort(); 
     }
 
-    if ( wait(&status) ) {
+    if ( wait(&status) != pid ) {
         err_sys("wait error");
     }
     pr_exit(status);
@@ -36,7 +54,7 @@ int main(void)
         status /= 0; 
     }
 
-    if ( wait(&status) ) {
+    if ( wait(&status) != pid ) {
         err_sys("wait error");
     }
     pr_exit(status);
