0236fc5bea76fdeebabccda9c21d7e9023e888e4
whj2819 <whj2819@gmail.com>
2012-07-15 21:31:01 +0800

    just comment.

--- unix/chp8/demo10.c
+++ unix/chp8/demo10.c
@@ -1,3 +1,21 @@
-#inlcude "apue.h"
+#include "apue.h"
+#include <sys/wait.h>
 
+int main(void)
+{
+    pid_t pid;
 
+    if ( (pid = fork() ) < 0) {
+        err_sys("fork error.");
+    } else if (0 == pid) {
+        if (execl("testinterp","testinterp","myarg1","MY ARG2",(char*)0) < 0) {
+            err_sys("execl error.");
+        }
+    }
+
+    if (waitpid(pid,NULL,0) < 0) {
+        err_sys("waitpid error");
+    }
+
+    exit (0);
+}
