452b5833ee5096cfb6cddcd8fd82d04cebd26c7a
whj2819 <whj2819@gmail.com>
2012-07-15 18:00:48 +0800

    nouse

--- unix/chp8/demo8.c
+++ unix/chp8/demo8.c
@@ -1,3 +1,33 @@
 #include "apue.h"
+#include <sys/wait.h>
 
+char *env_init[] = { "USER=unknown","PATH=/tmp",NULL };
 
+int main(void)
+{
+    pid_t pid;
+
+    if ( (pid = fork()) < 0 ) {
+        err_sys("fork error.");
+    } else if ( 0 == pid ) {
+        if ( execle("echoall","alex","myarg1",
+                    "MY ARG2",(char*)0,env_init) < 0) {
+            err_sys("ececle error.");
+        }
+    }
+
+    if (waitpid(pid,NULL,0) < 0) {
+        err_sys("wait error.");
+    }
+
+    printf("\n");
+    if ( (pid = fork()) < 0 ) {
+        err_sys("fork error.");
+    } else if ( 0 == pid ) {
+        if ( execlp("./echoall","echoall","only one arg", (char*)0) < 0) {
+            err_sys("ececlp error.");
+        }
+    }
+
+    exit (0);
+}
--- unix/chp8/echoall.c
+++ unix/chp8/echoall.c
@@ -14,4 +14,5 @@ int main(int argc,char *argv[])
         printf("%s\n",*ptr);
     }
 
+    return 0;
 }
