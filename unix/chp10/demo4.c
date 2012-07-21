acf4a698290ca3c5500bb9819785780c2f420c4a
whj2819 <whj2819@gmail.com>
2012-07-21 16:44:41 +0800

    nouse.

---
+++ unix/chp10/demo4.c
@@ -0,0 +1,18 @@
+#include <signal.h>
+#include <unistd.h>
+
+static void sig_alrm(int signo)
+{
+    /*nothing to do,just return to wake up the pause */
+}
+
+unsigned int sleep1(unsigned int nsecs)
+{
+    if (signal(SIGALRM,sig_alrm) == SIG_ERR) {
+        return (nsecs);
+    }
+
+    alarm(nsecs);
+    pause();
+    return(alarm(0));
+}
