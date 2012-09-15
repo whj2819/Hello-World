f69fa59b1bdf84f14a28a3ae08ec0365502b321b
whj2819 <whj2819@gmail.com>
2012-09-15 11:11:01 +0800

    rename unix-->apue

--- apue/chp19/demo5.c
+++ apue/chp19/demo5.c
@@ -27,4 +27,31 @@ int main(int argc,char *argv)
     struct winsize size;
 
     interactive = isatty(STDIN_FILENO);
+    ignoreeof = 0;
+    verbose = 0;
+    driver = NULL;
+
+    opterr = 0;
+
+    while (( c = getopt(argc,argv,OPTSTR)) != EOF) {
+
+        switch (c) {
+        case 'd':
+            driver = optarg;
+            break;
+        case 'e':
+            noecho = 1;
+            break;
+        case 'i':
+            ignoreeof = 1;
+            break;
+        case 'n':
+            interactive = 0;
+            break;
+        case 'v':
+            break;
+        case '?':
+            break;
+
+    }
 
