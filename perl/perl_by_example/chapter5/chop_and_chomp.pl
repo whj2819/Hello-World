7ab0c8d8c9f12a4f6f8c4d6662053ba722ff8c2b
whj2819 <whj2819@gmail.com>
2012-10-04 14:33:33 +0800

    nouse

---
+++ perl/perl_by_example/chapter5/chop_and_chomp.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use warnings;
+
+@line = ("red","green","orange");
+chop(@line);
+print "@line \n";
+
+@line = ("red","green","orange");
+chomp(@line);
+print "@line \n";
---
+++ perl/perl_by_example/chapter5/read_from_stdin.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+use warnings;
+
+$number = read(STDIN,$favorite,10);
+print "You just typed:$favorite \n";
+print "The number of bytes read was $number.\n";
