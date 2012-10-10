6a58f67989c0684d3b2bcc3bfc3b9d5fcfd214b8
whj2819 <whj2819@gmail.com>
2012-10-10 17:23:09 +0800

    nouse

--- perl/perl_by_example/chapter11/pointer.pl
+++ perl/perl_by_example/chapter11/pointer.pl
@@ -2,6 +2,32 @@
 
 use warnings;
 
-$name = 5;
-$p = \$name;
-print "$p \n";
+@toys = qw( Buzlightyear Woody Thomas Pokemon );
+$num = @toys;
+%movies = (
+    "Toy Story" => "US",
+    "Thomas" => "England",
+    "Pokemon" => "Japan",
+);
+
+$ref1 = \$num;
+$ref2 = \@toys;
+$ref3 = \%movies;
+
+print "There are $$ref1 toys.\n";
+print "There are @$ref2 .\n";
+while (($key,$value) = each(%$ref3)) {
+    print "$key -- $value \n";
+}
+
+print "His favorite toys are $ref2->[0] and $ref2->[3] \n";
+print "The Pokemon movie was made in $ref3->{'Pokemon'}. \n";
+
+print "*" x 80,"\n\n\n";
+
+$num = 5;
+$p = \$num;
+print 'The address assigned $p is ',$p,"\n";
+print "The value stored at that address is $$p \n";
+
+
