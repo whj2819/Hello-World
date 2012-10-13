70cbcd203763d8ddb0e2c025a07c3ee8937ff754
whj2819 <whj2819@gmail.com>
2012-10-13 14:08:54 +0800

    reverse

---
+++ perl/Perl_Cookbook/Hashes/invert.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+
+my %surname = (
+    "Mickey"    => "Mantle",
+    "Babe"      => "Ruth"
+);
+
+my $first_name = reverse %surname;
+print $first_name{"Mantle"};
---
+++ perl/perl_by_example/chapter5/reverse.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @names = qw(Bob Dan Tom Guy);
+print "@names \n";
+
+my @reversed = reverse @names;
+
+print "@reversed \n";
