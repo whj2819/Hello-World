8513b1a1987a9d9e0eba570a28d2c0c50def49de
whj2819 <whj2819@gmail.com>
2012-10-13 10:01:59 +0800

    delete chp1

--- perl/chp1/prob03.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-
-
-$a = "alpha";
-$b = "omega";
-($a,$b) = ($b,$a);
-print "$a ____ $b \n";
--- perl/chp1/prob05.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-
-use charnames ':full';
-
-print "\N{GREEK CAPITAL LETTER DELTA} is called delta. \n";
-
