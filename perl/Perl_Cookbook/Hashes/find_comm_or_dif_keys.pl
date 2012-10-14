2cc4b7e058229dd57ea3ed0d454b7628b33d667a
whj2819 <whj2819@gmail.com>
2012-10-14 21:33:40 +0800

    nouse

--- perl/Perl_Cookbook/Hashes/find_comm_or_dif_keys.pl
+++ perl/Perl_Cookbook/Hashes/find_comm_or_dif_keys.pl
@@ -3,3 +3,27 @@
 use strict;
 use warnings;
 
+my %citrus_color = (
+    Lemon    => "yellow",
+    Orange  => "orange",
+    Lime    => "green"
+);
+
+my %food_color = (
+    Apple    => "red",
+    Banana   => "yellow",
+    Lemon    => "yellow",
+    Carrot   => "orange"
+);
+
+# build up a list of no-citrus foods;
+
+my @non_citrus = ();
+
+foreach (keys %food_color) {
+    #push (@non_citrus,$_)   unless $citrus_color{$_};
+    push (@non_citrus,$_)   if $citrus_color{$_};
+
+}
+
+print "@non_citrus \n\n\n";
