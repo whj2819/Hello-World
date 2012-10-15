6bee417d5520d1b2d378de57b7d3f2e67d56af4c
root <root@fedora.domain>
2012-10-15 23:39:24 +0800

    nouse

--- perl/testcase/control.pl
+++ perl/testcase/control.pl
@@ -8,18 +8,17 @@ my $package_name = "$$.cap";
 my $dump_all_of_pack = "tcpdump -i p2p1 -w /tmp/$package_name -c 10000 -s 1600 ";
 my $dump_part_of_pack = $dump_all_of_pack;
 
+
 if ($args[0] eq '--start') {
     my $rv;
 
-    if (! $args[1]) { # args --mac == NULL;
-        usage();
-        exit 1;
-    }
+   # if (! $args[1]) { # args --mac == NULL;
+   #     usage();
+   #     exit 1;
+   # }
 
     $rv = tcpdump_start();
-    if ($rv) {
-        show_error();
-    } else {
+    if (! $rv) {
         show_info();
     }
     exit 0;
@@ -32,12 +31,12 @@ if ($args[0] eq '--start') {
 }
 
 sub tcpdump_start {
-    my $rv;
+    my $rv= 0;
     my $stbmac = 0;
 
     if ($args[2]) { # IPTMAC is not null;
         $rv = stbmac_find(\$stbmac);
-        print "\$stbmac:$stbmac \n";
+        print "\$stbmac:$stbmac \$rv:$rv \n";
         while ($rv) { 
             $rv = stbmac_find(\$stbmac);
             select(undef,undef,undef,0.5);# sleep 500ms 
@@ -61,20 +60,25 @@ sub stbmac_find {
     my $iptmac = $args[2];
     my $stbmac = shift(@_);
     my @line= split("\n\n",`cat stb_info.txt`);
-    my $ipdmac_match = 0;
+    my $match = 0;
 
     print "*" x 80,"\n";
     foreach (@line) {
         if ($_ =~ /ipt_id\=(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2})/io) {
             next if $1 ne $iptmac;
-            if (defined $$stbmac ) {
-                    return -1;
-             }
+            $match = 1;# iptmac exist;
             if ($_ =~ /stb_mac\=(([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2})/io) {
                 $$stbmac = $1;
                 return 0;
             }
-        }
+             if (defined $$stbmac ) {
+                    return -1;
+             }
+        } 
+    }
+    if (! $match) {
+         print "This IPT MAC is not exist.\n";
+         exit 1;
     }
 }
 
@@ -111,11 +115,12 @@ sub get_tcpdump_pid {
 }
 
 sub usage {
+    print "*" x 80,"\n";
     print "control netcap --start  [ --mac  IPTMAC ] \n\n";
+    print "*" x 80,"\n";
 }
 
 sub show_error {
-    print "show_error:$! \n";
     return 0;
 }
 
