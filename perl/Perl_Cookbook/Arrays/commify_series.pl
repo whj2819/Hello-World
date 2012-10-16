ead98a51cd7307fa3990bcb1860a2fa5118a9104
whj2819 <whj2819@gmail.com>
2012-10-16 23:28:50 +0800

    nouse

--- perl/Perl_Cookbook/Arrays/commify_series.pl
+++ perl/Perl_Cookbook/Arrays/commify_series.pl
@@ -3,8 +3,28 @@
 use strict;
 use warnings;
 
-my @list = (
+my @lists = (
     ['just one thing'],
-    [qw(Mutt Jeff],
+    [qw(Mutt Jeff)],
+    [qw(Peter Paul Mary)],
 );
 
+my $aref = 0;
+
+foreach $aref (@lists) {
+    print "The list is: " . commify_series(@$aref) . ".\n";
+}
+print "\n\n";
+print "*" x 80,"\n";
+
+my @list = qw(one two three);
+print "The list is: " . commify_series(@list) . ".\n";
+
+sub commify_series {
+    my $sepchar = grep(/,/ =>@_) ? ";" : ",";
+    (@_ == 0) ? ''                                :
+    (@_ == 1) ? $_[0]                             :
+    (@_ == 2) ? join(" and ",@_)                  :
+                join("$sepchar ",@_[0 ..($#_-1)],"and $_[-1]");
+
+}
