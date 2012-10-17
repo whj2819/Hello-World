e77152749a1fee9933aaaae7a8b23ccc25d871fa
whj2819 <whj2819@gmail.com>
2012-10-17 17:17:04 +0800

    nouse

--- perl/testcase/Getopt.pl
+++ perl/testcase/Getopt.pl
@@ -7,15 +7,16 @@ my $all = '';
 my $tag = ''; 
 my $libfiles= ''; 
 
+
 GetOptions (
     'verbose+'   => \$verbose,# option variable with default value (false)
     'all'        => \$all,
     'tag=s'      => \$tag,
-    'librarys=s' =>\@libfiles
+    'librarys=s@' =>\$libfiles
 );
 
 #@libfiles = split(/,/,join(',',@libfiles));
 print "\$verbose:$verbose \n";
 print "\$all:$all\n";
 print "\$tag:$tag\n";
-print "\@libfiles:@libfiles\n";
+print "\@libfiles:$libfiles->[0]\n";
