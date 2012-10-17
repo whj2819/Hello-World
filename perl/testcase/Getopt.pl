416bfd26836f1cfa6782b2828461aa80750314e0
whj2819 <whj2819@gmail.com>
2012-10-17 17:08:06 +0800

    option have multiple values by add a @

--- perl/testcase/Getopt.pl
+++ perl/testcase/Getopt.pl
@@ -4,11 +4,18 @@ use Getopt::Long;
 
 my $verbose = '';
 my $all = '';
+my $tag = ''; 
+my $libfiles= ''; 
 
 GetOptions (
-    'verbose!'   => \$verbose,# option variable with default value (false)
-    'all'       => \$all
+    'verbose+'   => \$verbose,# option variable with default value (false)
+    'all'        => \$all,
+    'tag=s'      => \$tag,
+    'librarys=s' =>\@libfiles
 );
 
+#@libfiles = split(/,/,join(',',@libfiles));
 print "\$verbose:$verbose \n";
 print "\$all:$all\n";
+print "\$tag:$tag\n";
+print "\@libfiles:@libfiles\n";
