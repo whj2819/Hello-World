c57b3bd4c8ba11e78fda7418a3c24cbfae82189e
whj2819 <whj2819@gmail.com>
2012-10-17 16:46:19 +0800

    getopt study

--- perl/testcase/Getopt.pl
+++ perl/testcase/Getopt.pl
@@ -2,25 +2,3 @@
 
 use Getopt::Long;
 
-sub start{
-    print "start+++++++++++ \n\n";
-}
-
-sub stop{
-    print "stop_______ \n\n";
-}
-
-my $mac = '';
-my ($cmd,@args) = @ARGV;
-
-GetOptions(
-    \%opts,
-    'start' => sub{start()},
-    'mac=s' => \$mac,
-    'stop'      => sub{stop()}
-);
-
-print "****\@ARGV: @ARGV \n";
-print "****\$cmd:$cmd   \@args:@args \n";
-print "MAC: $mac \n";
-
