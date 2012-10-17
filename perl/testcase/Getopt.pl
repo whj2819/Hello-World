155855a06d1266416f5e323a3a8a01b81d55d38d
whj2819 <whj2819@gmail.com>
2012-10-17 17:29:54 +0800

    Options take multiple values at once

--- perl/testcase/Getopt.pl
+++ perl/testcase/Getopt.pl
@@ -6,17 +6,15 @@ my $verbose = '';
 my $all = '';
 my $tag = ''; 
 my $libfiles= ''; 
+my @coor = () ; 
 
 
 GetOptions (
     'verbose+'   => \$verbose,# option variable with default value (false)
     'all'        => \$all,
     'tag=s'      => \$tag,
-    'librarys=s@' =>\$libfiles
+    'librarys=s@' =>\$libfiles,
+    'coordinates=f{2}' =>\@coor
 );
 
-#@libfiles = split(/,/,join(',',@libfiles));
-print "\$verbose:$verbose \n";
-print "\$all:$all\n";
-print "\$tag:$tag\n";
-print "\@libfiles:$libfiles->[0]\n";
+print "\@coordinates:@coor\n";
