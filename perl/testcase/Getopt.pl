3e633d33d42fbe4a7d497ebfc59bbb6e953232eb
whj2819 <whj2819@gmail.com>
2012-10-17 16:49:37 +0800

    nouse

--- perl/testcase/Getopt.pl
+++ perl/testcase/Getopt.pl
@@ -2,3 +2,13 @@
 
 use Getopt::Long;
 
+my $verbose = '';
+my $all = '';
+
+GetOptions (
+    'verbose!'   => \$verbose,# option variable with default value (false)
+    'all'       => \$all
+);
+
+print "\$verbose:$verbose \n";
+print "\$all:$all\n";
