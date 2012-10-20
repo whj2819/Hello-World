de65e1a97f01f929beccc0cd5a24b93310d87640
whj2819 <whj2819@gmail.com>
2012-10-20 16:08:30 +0800

    nouse

--- perl/Perl_Cookbook/subroutines/current_func_name.pl
+++ perl/Perl_Cookbook/subroutines/current_func_name.pl
@@ -4,6 +4,7 @@
 #use warnings;
 
 
+=pon
 ($package,$filename,$line,$subr,$has_args,$wantarray,
     $evaltext,$is_require,$hints,$bitmask
 ) = caller($i);
@@ -18,3 +19,25 @@ print "\$evaltext: $evaltext\n";
 print "\$is_require: $is_require\n";
 print "\$hints: $hints\n";
 print "\$bitmask: $bitmask\n";
+=cut
+my $me = whoami();
+my $him = whowasi();
+
+sub whoami {
+    (caller(2))[3]
+}
+
+sub whowasi {
+    (caller(2))[3]
+}
+
+sub test {
+    tmp();
+}
+
+sub tmp {
+    my $test = whoami();
+    print "\$test:$test\n";
+}
+
+test();
