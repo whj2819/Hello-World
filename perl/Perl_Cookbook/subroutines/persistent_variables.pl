bbbeae46df1b3ca4c06a25c5ee34446196ff40c3
whj2819 <whj2819@gmail.com>
2012-10-20 15:37:13 +0800

    nouse

---
+++ perl/Perl_Cookbook/subroutines/persistent_variables.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+{
+    my $counter;
+
+    sub next_counter{
+        return $counter++;
+    }
+
+}
+
+$cnt = next_counter();
+print "\$cnt:$cnt\n";
+$cnt = next_counter();
+print "\$cnt:$cnt\n";
+$cnt = next_counter();
+print "\$cnt:$cnt\n";
