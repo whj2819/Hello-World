8158aeccf7ea201d18cc7301daca018ad8f6adde
whj2819 <whj2819@gmail.com>
2012-10-23 22:35:40 +0800

    nouse

--- perl/programming_perl/package/package.pl
+++ perl/programming_perl/package/package.pl
@@ -2,3 +2,11 @@
 
 use strict;
 use warnings;
+
+foreach my $symname  (sort keys %main::) {
+    local *sym = $main::{$symname};
+
+    print "\$$symname is defined\n" if defined $sym;
+    print "\@$symname is nonnull \n" if @sym; 
+    print "\%$symname is nonnull \n" if %sym; 
+}
