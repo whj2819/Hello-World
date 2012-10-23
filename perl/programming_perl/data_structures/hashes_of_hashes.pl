6d3fdebf263df491e86e5de54c99733273c72f26
whj2819 <whj2819@gmail.com>
2012-10-23 21:09:08 +0800

    hashes of hashes

--- perl/programming_perl/data_structures/hashes_of_hashes.pl
+++ perl/programming_perl/data_structures/hashes_of_hashes.pl
@@ -3,3 +3,34 @@
 use strict;
 use warnings;
 
+
+my %HoH = (
+    flintstones => {
+        husband     => "fred",
+        pal         => "barney",
+    },
+    jetsons => {
+        husband    => "george",
+        wife       => "jane",
+        "his boy"  => "elroy",
+    },
+    simpsons   => {
+        husbond    => "homer",
+        wife       => "marge",
+        kid        => "bart",
+    },
+);
+
+$HoH{mash} = {
+    captain    => "pierce",
+    major       => "burns",
+    corporal   => "radar",
+};
+
+for my $family (keys %HoH) {
+    print "$family:\n";
+    for my $role (keys $HoH{$family}) {
+        print "$role = $HoH{$family}{$role}";
+    }
+    print "\n\n";
+}
