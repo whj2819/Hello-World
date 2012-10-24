a95927c4f722f185c53beb17054bff830455a635
whj2819 <whj2819@gmail.com>
2012-10-24 11:08:00 +0800

    modules test code

--- perl/programming_perl/modules/Bestiary.pm
+++ perl/programming_perl/modules/Bestiary.pm
@@ -1,15 +1,19 @@
-pacakge Bestiary;
+package Bestiary;
 require Exporter;
 
 our @ISA = qw(Exporter);
 our @EXPORT = qw(camel);
-our @EXPORT_OK = qw($weight);
+our @EXPORT_OK = qw(test $weight);
 our $version = 1.00;
 
 sub camel {
-    print "One-hump dromedary"
+    print "One-hump dromedary \n";
 }
 
-$weght = 1024;
+sub test {
+    print "tesst _______________ \n";
+}
+
+$weight = 1024;
 
 1; # make require happy
---
+++ perl/programming_perl/modules/test_bestiary.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use Bestiary qw(camel test $weight);
+
+
+camel();
+test();
+
+my $w = $weight;
+print "$w ++++---- \n";
+
+
