345965b90d02cfbbf8ac11cc506587c16f17063b
whj2819 <whj2819@gmail.com>
2012-10-24 14:19:34 +0800

    nouse

--- perl/Perl_Cookbook/ref_and_recoeds/manipulate_array_by_ref.pl
+++ perl/Perl_Cookbook/ref_and_recoeds/manipulate_array_by_ref.pl
@@ -5,5 +5,9 @@ use warnings;
 
 my @array = (1,2,3,4,5,6,7,8);
 my $aref = \@array;
+my $ano_array= [1,2,3,4,5,6,7,8];
+my $ano_copy= [@array];
 
 print $aref->[5],"\n";
+print $ano_array->[3],"\n";
+print $ano_copy->[5],"\n";
