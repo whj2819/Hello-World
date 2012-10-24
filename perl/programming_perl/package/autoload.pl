9449aa95484f0a800424a4f1b98372551e134de4
whj2819 <whj2819@gmail.com>
2012-10-24 10:28:23 +0800

    closures autoload

--- perl/programming_perl/package/autoload.pl
+++ perl/programming_perl/package/autoload.pl
@@ -1,9 +1,24 @@
 #!/usr/bin/perl
 
+# closure
 sub AUTOLOAD {
-    our $AUTOLOAD;
-    warn "Attempt to call $AUTOLOAD failed .\n";
+    my $name = our $AUTOLOAD;
+    *$AUTOLOAD = sub {
+        print "I see $name(@_) \n";
+    };
+    goto &$AUTOLOAD;
 }
 
-blarg(10);
-print "Still alive! \n";
+blarg(30);
+blarg(40);
+blarg(50);
+
+
+__END__
+sub AUTOLOAD {
+    our $AUTOLOAD;
+
+    return "I see $AUTOLOAD(@_) \n";
+}
+
+print blarg(10);
