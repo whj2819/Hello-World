a5f952f9656436737806cd8a72172faa23ef79f3
whj2819 <whj2819@gmail.com>
2012-10-24 15:52:01 +0800

    hash of arrays

--- perl/Perl_Cookbook/ref_and_recoeds/hash_of_arrays.pl
+++ perl/Perl_Cookbook/ref_and_recoeds/hash_of_arrays.pl
@@ -7,5 +7,14 @@ my %HoA = (
     "a key"    => [3,4,5],
 );
 
-my @values = @{ $HoA{"a key"} };
-print "@values\n";
+
+push @{ $HoA{"a key"} },12;
+
+# Be warned that this:
+my @values = @{ $HoA{number} };
+print "222 @values\n";
+
+# you must do this instead:
+@values = exits ( $HoA{number}) 
+                ?  @{ $HoA{number} }
+                : ( );
