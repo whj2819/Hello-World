ea2bde9da7159ebe84c0ec77288bab658ab3d671
whj2819 <whj2819@gmail.com>
2012-10-24 16:04:29 +0800

    taking reference to hash

---
+++ perl/Perl_Cookbook/ref_and_recoeds/ref_to_hash.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my %hash = (
+    key    => "test",
+    key2    => "test2",
+);
+
+my $href = \%hash;
+
+print "$href->{key} \n";
+
+my $anon_hash = {
+    key1    => "value1",
+    key2    => "value2",
+};
+
+print "$anon_hash->{key2} \n";
+
+my $anon_copy = { %hash };
+print "$anon_copy->{key2} \n";
+
+
+
