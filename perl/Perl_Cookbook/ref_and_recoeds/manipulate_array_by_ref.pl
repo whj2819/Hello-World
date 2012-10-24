0e4e0abee17023146df2236035557e6afba3475c
whj2819 <whj2819@gmail.com>
2012-10-24 14:41:57 +0800

    nouse

--- perl/Perl_Cookbook/ref_and_recoeds/manipulate_array_by_ref.pl
+++ perl/Perl_Cookbook/ref_and_recoeds/manipulate_array_by_ref.pl
@@ -8,9 +8,21 @@ my $aref = \@array;
 my $ano_array= [1,2,3,4,5,6,7,8];
 my $ano_copy= [@array];
 
+
+
+
+
+=pon
 print $aref->[5],"\n";
 print $ano_array->[3],"\n";
 print $ano_copy->[5],"\n";
 
 push(@$ano_array,11);
 print $ano_array->[8],"\n";
+
+my $last_idx = $#{$aref};
+print "\$last_idx:$last_idx \n";
+
+my $num_items = @$aref;
+print "\$num_items:$num_items\n";
+=cut
