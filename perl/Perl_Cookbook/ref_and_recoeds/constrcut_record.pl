218c2605519e05dcef810a54b504aaf4f1dbaaf0
whj2819 <whj2819@gmail.com>
2012-10-25 11:29:10 +0800

    nouse

--- perl/Perl_Cookbook/ref_and_recoeds/constrcut_record.pl
+++ perl/Perl_Cookbook/ref_and_recoeds/constrcut_record.pl
@@ -2,3 +2,12 @@
 
 use strict;
 use warnings;
+
+my $recode = {
+    NAME    => "Jashon",
+    EMPNO   => 132,
+    TITLE   => "deputy peon",
+    AGE     => 23,
+    SALARY  => 37_000,
+    PALS    => ["Norbert","Rhys","Phineas"],
+};
