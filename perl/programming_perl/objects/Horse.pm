b26d10030367eec805b90db06269bbc52349a1a2
whj2819 <whj2819@gmail.com>
2012-10-31 23:10:57 +0800

    nouse

--- perl/programming_perl/objects/Horse.pm
+++ perl/programming_perl/objects/Horse.pm
@@ -15,4 +15,26 @@ sub new {
     return $self;
 }
 
+sub clone {
+    my $model = shift(@_);
+    print "$model------ @_ \n";
+    my $self = $model->new(%$model,@_);
+
+    while (my($k,$v) = each %$model) {
+        print "++++\$k:$k,\$v:$v \n";
+    }
+
+    return $self;
+}
+
+sub __print {
+
+    my @param = @_;
+
+    print "___ @param \n";
+}
+
+
+
 1;# make requrie happy
+
