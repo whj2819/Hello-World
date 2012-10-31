1ca9d3d1a0d6c5e6fc5613514fe1c65dea2002d3
whj2819 <whj2819@gmail.com>
2012-10-31 17:23:51 +0800

    nouse

--- perl/programming_perl/objects/Horse.pm
+++ perl/programming_perl/objects/Horse.pm
@@ -4,7 +4,12 @@ package Horse;
 sub new {
     my $this = shift;
     my $class = ref($this) || $this;
-    my $self = {@_};
+    my $self = {
+        color   => "bay",
+        legs    => 4,
+        owner   => undef,
+        @_,
+    };
 
     bless ($self,$class);
     return $self;
