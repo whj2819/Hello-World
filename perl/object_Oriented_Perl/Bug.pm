5c81a61464746ad62de438b3fd7ba6363ab981a6
whj2819 <whj2819@gmail.com>
2012-11-03 20:30:20 +0800

    nouse

--- perl/object_Oriented_Perl/Bug.pm
+++ perl/object_Oriented_Perl/Bug.pm
@@ -1,5 +1,18 @@
-package Bug
+package Bug;
 
+use strict;
+
+sub new {
+    print "$_[0],$_[1],$_[2],$_[3] \n\n";
+    bless {
+        _id => $_[1],
+        _type => $_[2],
+        _descr => $_[3],
+    },$_[0];
+}
+
+
+=pon
 sub new {
     my $class = $_[0];
     my $objref = {
@@ -8,6 +21,9 @@ sub new {
         _descr => $_[3],
     };
     bless $objref,$class;
-
+    print "$_[0],$_[1],$_[2],$_[3] \n\n";
     return $objref;
 }
+
+=cut
+1;# make require happy
