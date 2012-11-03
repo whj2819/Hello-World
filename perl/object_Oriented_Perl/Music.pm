cc047e7bac012320aaa29d6f8bdcd0d4091d221c
whj2819 <whj2819@gmail.com>
2012-11-03 21:00:54 +0800

    nouse

--- perl/object_Oriented_Perl/Music.pm
+++ perl/object_Oriented_Perl/Music.pm
@@ -36,3 +36,20 @@ sub ISBN{
 sub tracks{
     $_[0] ->{_tracks}
 }
+
+sub location {
+    my ($self,$shelf,$room) = @_;
+
+    $self->{_room} = $room if $room;
+    $self->{_shelf} = $shelf if $shelf;
+
+    return ($self->{_room},$self->{_shelf});
+
+}
+
+sub rating {
+    my ($self,$rating)  = @_;
+
+    $self->{_rating} = $rating if defined $rating ;
+    return $self->{_rating};
+}
