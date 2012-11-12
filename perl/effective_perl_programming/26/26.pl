bc7bf02269141ee9c3b844dbf73b39becd583558
whj2819 <whj2819@gmail.com>
2012-11-12 21:14:32 +0800

    sum and reduce func

--- perl/effective_perl_programming/26/26.pl
+++ perl/effective_perl_programming/26/26.pl
@@ -2,16 +2,16 @@
 
 use strict;
 use warnings;
-use List::Util qw(max maxstr);
+use List::Util qw(first max maxstr min minstr reduce shuffle sum);
 
 
 my $max_number = max(0 .. 99);
 my $max_str = maxstr( qw(Fido Spot Rower));
+my $sum = sum(0 .. 100);
+#my $sum = reduce{$a * $b} 1 .. 5;
 
 print "\$max_number:$max_number \n";
 print "\$max_str:$max_str \n";
-
-
-
+print "\$sum:$sum\n";
 
 
