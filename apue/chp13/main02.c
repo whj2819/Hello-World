53ec04ec93730906ae9791c26e4ce2a6db776a40
whj2819 <whj2819@gmail.com>
2012-11-13 22:41:03 +0800

    nouse

--- apue/chp13/main02.c
+++ apue/chp13/main02.c
@@ -11,3 +11,26 @@ reread(void)
 {
 
 }
+
+void *
+thr_fn(void *arg)
+{
+    for (;;) {
+        err = sigwait(&mask,&signo);
+        syslog(LOG_ERR,"sigwait failed");
+        exit(1);
+    }
+
+    switch (signo) {
+    case SIGHUP:
+        syslog(LOG_INFO,"Re-reading configuration file");
+        reread();
+        break;
+    case SIGTERM:
+        syslog(LOG_INFO,"got SIGTERM;exiting");
+        exit(0);
+    default:
+        syslog(LOG_INFO,"unexpected signal %d \n",signo);
+    }
+
+}
