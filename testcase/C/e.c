c94e3cdb5be1b28d11a0bfc5324c9a3ddebe3554
whj2819 <whj2819@gmail.com>
2013-01-03 23:35:44 +0800

    简单实现,先能够测试

--- testcase/C/e.c
+++ testcase/C/e.c
@@ -6,21 +6,41 @@ int
 main(int argc,char **argv)
 {
     u16 a[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    u16 tmp[3][4];
     int i,j;
-    int row = 4,col = 3;
+    int row = 3,col = 4;
+    u16 *p  = a;
 
     for (i=0; i<12; i++) 
         printf("%d ",a[i]);
     printf("\n\n\n");
 
-
-    for (i=0;i<col;i++) {
-        for(j=row;j>=0;j--) {
-            printf("%d ",*(a+j*(col-1)+1+i));
+    for (i = 0;i<row;i++) {
+        for(j = 0;j<col;j++) {
+            tmp[i][j] = *(p+j);
+        }
+        p += col;;
+    }
+    for (i = 0;i<row;i++) {
+        for(j = 0;j<col;j++) {
+            printf("%d ",tmp[i][j]);
         }
         printf("\n");
     }
+        printf("\n");
+        printf("\n");
+        printf("\n");
 
+        p = a;
+
+    for (i = 0;i<col;i++) {
+        for(j = row-1;j>=0;j--) {
+             *(p+i)= tmp[j][i];
+             printf("%d ",*(p+i));
+        }
+        p += 4;
+        printf("\n");
+    }
 
 
 
