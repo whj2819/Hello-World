35662ddc01333d82b31a96467f396ce4a76fa28b
whj2819 <whj2819@gmail.com>
2013-01-06 16:24:27 +0800

    nouse

--- unpv13e/chp11/daytimetcpcli1.c
+++ unpv13e/chp11/daytimetcpcli1.c
@@ -1,4 +1,4 @@
-#inlcude "unp.h"
+#include "unp.h"
 
 
 int
@@ -20,18 +20,37 @@ main(int argc,char **argv)
 	if ( (hp = gethostbyname(argv[1])) == NULL) {
 		if (inet_aton(argv[1],&inetaddr) == 0) {
 			err_quit("hostname error for %s:%s \n",argv[1],
-						hsterror(h_errno));
+						hstrerror(h_errno));
 		} else {
 		    inetaddrp[0] = &inetaddr;
 			inetaddrp[1] = NULL;
 			pptr = inetaddrp;
 		}
 	} else {
-        pptr = (in_addr **)hp->h_addr_list;
+        pptr = (struct in_addr **)hp->h_addr_list;
 	}
 
 	if ( (sp = getservbyname(argv[2],"tcp")) == NULL)
 		err_quit("getservbyname error for %s ",argv[2]);
+
+	for ( ; *pptr != NULL;pptr++) {
+        sockfd = Socket(AF_INET,SOCK_STREAM,0);
+		bzero(&servaddr,sizeof(servaddr));
+
+		servaddr->sin_family = AF_INET;
+		servaddr->port = sp->s_port;
+		memcpy(&servaddr.sin_addr,*pptr,sizeof(struct in_addr));
+		printf("trying %s \n",Sock_ntop(SA *)&servaddr,sizeof(servaddr));
+
+		if (connect(sockfd,(SA*)&servaddr,sizeof(servaddr)) == 0) 
+			break;
+		err_quit("connect error");
+		close(sockfd);
+	}
+
+	if (*pptr == NULL) 
+		err_quit("unable to connect");
+	
 
 	
 }
