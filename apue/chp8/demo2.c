8c49b6e1271d060f9e73172bbd2fe0b604454f4d
whj2819 <whj2819@gmail.com>
2013-05-09 22:54:24 +0800

    nouse

--- apue/chp8/demo2.c
+++ apue/chp8/demo2.c
@@ -15,17 +15,13 @@ int main(void)
         err_sys("write error");
     }
     
-    printf("before fork \n");
-    if (( pid = fork()) < 0) {
+    printf("before vfork \n");
+    if (( pid = vfork()) < 0) {
         err_sys("fork error \n");
     } else if( 0 == pid ) { /*child */
         glob++;
         var++;
-        //printf("I'm child process!\n");
-       // printf("pid = %d ppid= %d \n",getpid(),getppid());
-    } else {
-        //printf("I'm partent process!\n");
-        sleep(2);
+        _exit(0);
     }
     printf("pid = %d glob = %d var = %d \n",getpid(),glob,var);
 
