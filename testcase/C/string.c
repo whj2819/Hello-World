136dc7ece59f525762cb564f9f9560e0b293801d
whj2819 <whj2819@gmail.com>
2013-06-19 21:47:22 +0800

    nouse

--- testcase/C/string.c
+++ testcase/C/string.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 
 
 int
 main(int argc, char *argv[])
 {
+    char *error = "ERROR:";
+    char *error_message = "not enough memory";
+
+    char *buffer = (char *)malloc(strlen(error) + strlen(error_message) +1);
+
+    strcpy(buffer,error);
+    strcat(buffer,error_message);
+
+    printf("[%s] \n",buffer);
+    printf("[%s] \n",error);
+    printf("[%s] \n",error_message);
+
     return 0;
 
 }
