c77175f85a81514092c83b1ddfbebdb56f379b7d
whj2819 <whj2819@gmail.com>
2013-06-26 20:54:00 +0800

    shmread

---
+++ unpv22e/chap13/shmread.c
@@ -0,0 +1,33 @@
+#include "unpipc.h"
+
+
+int
+main(int argc, char *argv)
+{
+    int i,fd;
+    struct stat stat;
+    unsigned char c,*ptr;
+
+
+    if (argc != 2)
+        err_quit("usage:shmread <name>");
+
+    fd = Shm_open(argv[1],O_RDWR,FILE_MODE);
+    Fstat(fd,&stat);
+    ptr = Mmap(
+                NULL,
+                stat.st_size,
+                PROT_READ,
+                MAP_SHARED,
+                fd,
+                0
+            );
+
+    Close(fd);
+
+    for (i=0; i<stat.st_size;i++) 
+        if ( (c = *ptr++) != (i % 256) )
+            err_ret("ptr[%d] = %d",i,c);
+
+    exit (0);
+}
---
+++ unpv22e/chap13/shmwrite.c
@@ -0,0 +1,32 @@
+#include "unpipc.h"
+
+
+int
+main(int argc, char *argv)
+{
+    int i,fd;
+    struct stat stat;
+    unsigned char *ptr;
+
+
+    if (argc != 2)
+        err_quit("usage:shmwirte <name>");
+
+    fd = Shm_open(argv[1],O_RDWR,FILE_MODE);
+    Fstat(fd,&stat);
+    ptr = Mmap(
+                NULL,
+                stat.st_size,
+                PROT_READ | PROT_WRITE,
+                MAP_SHARED,
+                fd,
+                0
+            );
+
+    Close(fd);
+
+    for (i=0; i<stat.st_size;i++)
+        *ptr++ = i % 256;
+
+    exit (0);
+}
