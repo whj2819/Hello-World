e4a65e22f3c571af6eaaa8d03fdcbdaca013a543
whj2819 <whj2819@gmail.com>
2013-06-26 14:00:26 +0800

    server

--- unpv22e/chap4/server.c
+++ unpv22e/chap4/server.c
@@ -1,9 +1,29 @@
-#include ""
+#include "unpipc.h"
 
-
-
-int
-main(int argc, char *argv[])
+void
+server(int readfd, int writefd)
 {
+    int fd;
+    ssize_t n;
+    char buff[MAXLINE + 1];
 
+    if ( (n = Read(readfd,buff,MAXLINE)) < 0)
+        err_quit("end-of-file while reading pathname");
+    buff[n] = '\0';
+
+
+    if ( (fd = open(buff,O_RDONLY)) < 0) {
+
+        snprintf(buff + n,sizeof(buff) - n, ": can't open,%s\n",
+                strerror(errno));
+
+        n = strlen(buff);
+        Write(writefd,buff,n);
+    } else {
+        while ( (n = Read(fd,buff,MAXLINE)) > 0)
+            Write(writefd,buff,n);
+
+        Close(fd);
+    }
 }
+
