234b891fcdb388ec8ee8a1bfc7cf3bb5973b157f
whj2819 <whj2819@gmail.com>
2013-06-30 01:04:46 +0800

    类型长度

---
+++ testcase/C/length.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+int
+main(int argc, char *argv[])
+{
+    printf("sizeof(char)    :%d\n",sizeof(char));
+    printf("sizeof(short)    :%d\n",sizeof(short));
+    printf("sizeof(long int)    :%d\n",sizeof(long int));
+    printf("sizeof(float)    :%d\n",sizeof(float));
+    printf("sizeof(double)    :%d\n",sizeof(double));
+
+    return 0;
+}
