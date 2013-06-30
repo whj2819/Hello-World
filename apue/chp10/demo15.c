f23c7a39878cfa8342c3ecefd91b4031501f11da
whj2819 <whj2819@gmail.com>
2013-06-30 21:44:53 +0800

    nouse

--- apue/chp10/demo15.c
+++ apue/chp10/demo15.c
@@ -1,6 +1,37 @@
 #include "apue.h"
+#include <stdlib.h>
+#include <errno.h>
 
-static void sig_int(int);
+
+
+
+void pr_mask(const char *str)
+{
+    sigset_t sigset;
+    int errno_save;
+
+    errno_save = errno;
+    if (sigprocmask(0,NULL,&sigset) < 0)
+        err_sys("sigprocmask error");
+
+    printf("%s",str);
+
+    if (sigismember(&sigset, SIGINT)) err_sys("SIGINT ");
+    if (sigismember(&sigset, SIGQUIT)) err_sys("SIGQUIT ");
+    if (sigismember(&sigset, SIGUSR1)) err_sys("SIGUSR1 ");
+    if (sigismember(&sigset, SIGALRM)) err_sys("SIGALRM ");
+
+    /* remaining signals can go here */
+
+    printf("\n");
+    errno = errno_save;
+}
+
+static void
+sig_int(int signo)
+{
+    pr_mask("\n in sig_int :\n");
+}
 
 int
 main(void)
@@ -10,7 +41,7 @@ main(void)
     pr_mask("program start:");
 
     if (signal(SIGINT, sig_int) == SIG_ERR)
-        printf("sig sigint error\n");
+        err_sys("sig sigint error");
 
     sigemptyset(&waitmask);
     sigaddset(&waitmask, SIGUSR1);
@@ -18,22 +49,22 @@ main(void)
     sigaddset(&newmask, SIGINT);
 
     if ( sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
-        printf("sig block error \n");
+        err_sys("sig block error ");
 
     pr_mask("in critical region ");
 
+    if (sigsuspend(&waitmask) != -1)
+        err_sys("sigsuspend error");
+
+    pr_mask("after return from sigsuspend");
+
 
     if ( sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
-        printf(" sig_setmask error \n");
+        err_sys(" sig_setmask error ");
 
 
-    pr_mask("program exit");
-
-    return 0;
+    pr_mask("program exit :");
+    exit(0);
 }
 
-static void
-sig_int(int signo)
-{
-    pr_mask("\n in sig_int \n");
-}
+
--- testcase/C/signal/sigprocmask_test.c
+++ testcase/C/signal/sigprocmask_test.c
@@ -1,1 +1,36 @@
 #include <stdio.h>
+#include <signal.h>
+
+
+int
+main(int argc, char *argv[])
+{
+    sigset_t newmask ,oldmask;
+
+
+    sigemptyset(&newmask);
+    sigaddset(&newmask,SIGINT);
+
+
+    if( sigismember(&newmask,SIGINT))
+        printf("newmask SIGINT \n");
+
+    if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
+        printf("error ------------\n");
+
+    printf("===========================before \n");
+
+    if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+        printf("error ------------\n");
+
+
+
+
+
+
+    printf("===========================after\n");
+    if( sigismember(&oldmask,SIGINT))
+        printf("oldmask SIGINT \n");
+
+    return 0;
+}
