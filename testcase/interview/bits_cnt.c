5f81156becf22e99da87fc00b88266ca43619079
whj2819 <whj2819@gmail.com>
2013-07-17 15:47:15 +0800

    interview example

---
+++ testcase/interview/bits_cnt.c
@@ -0,0 +1,25 @@
+/*
+ * 计算 变量包含多少位1
+  *
+ */
+
+
+#include <stdio.h>
+
+int
+main(void)
+{
+    int cnt = 0;
+    int x = 7;
+
+    while (x) {
+
+        cnt ++;
+        x = x & (x-1);
+    }
+
+    printf("[%d]\n", cnt);
+
+    return 0;
+}
+
