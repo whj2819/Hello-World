bd689e9507ba475486b2c93630a856abfcd70abc
whj2819 <whj2819@gmail.com>
2013-07-20 20:09:15 +0800

    modefiy file name

--- apue/chp11/threadid.c
+++ apue/chp11/threadid.c
@@ -3,7 +3,8 @@
 
 pthread_t ntid;
 
-void printids(const char *s)
+void
+printids(const char *s)
 {
     pid_t pid;
     pthread_t tid;
@@ -14,20 +15,21 @@ void printids(const char *s)
             (unsigned int)tid,(unsigned int)tid);
 }
 
-void * thr_fn(void *arg)
+void *
+thr_fn(void *arg)
 {
     printids("new thread:");
     return ((void *)0);
 }
 
-int main(void)
+int
+main(void)
 {
     int err;
 
     err = pthread_create(&ntid,NULL,thr_fn,NULL);
-    if (err != 0) {
+    if (err != 0)
         err_quit("can't create thread:%s \n",strerror(err));
-    }
 
     printids("main thread:");
     sleep(1);
