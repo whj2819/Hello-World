e8f3fb48ee483a6e71aea0df9e278015d6594205
whj2819 <whj2819@gmail.com>
2013-10-04 14:38:36 +0800

    ref test

--- perl/programming_perl/chap8/ref.pl
+++ perl/programming_perl/chap8/ref.pl
@@ -1,2 +1,16 @@
-#!/usr/local/bin/perl
+#!/usr/bin/perl
 
+use strict;
+use warnings;
+
+
+my $scalar = 1;
+my @array = (1, 2, 3);
+my %hash = (
+    zdd => 30,
+    autumn => 27,
+);
+
+my $sref = \$scalar;
+my $aref = \@array;
+my $href = \%hash;
