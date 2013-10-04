138fa83edaa4a176915158be4bf120383f3bca4d
whj2819 <whj2819@gmail.com>
2013-10-04 21:30:48 +0800

    incr3

--- unpv22e/chap12/incr3.c
+++ unpv22e/chap12/incr3.c
@@ -1,23 +1,25 @@
 #include "unpipc.h"
 
-#define SEM_NAME "mysem"
+struct shared {
+    sem_t mutex;
+    int count;
+}shared;
 
 int
 main(int argc, char *argv[])
 {
-    int fd,i,nloop,zero = 0;
-    int *ptr;
-    sem_t *mutex;
+    int fd,i,nloop;
+    struct shared *ptr;
 
     if (argc != 3)
         err_quit("usage: incr2 <pathname> <#loops> \n");
     nloop = atoi(argv[2]);
 
     fd = Open(argv[1],O_RDWR | O_CREAT, FILE_MODE);
-    Write(fd, &zero,sizeof(int));
+    Write(fd, &shared,sizeof(struct shared));
     ptr = Mmap(
                       NULL,
-                      sizeof(int),
+                      sizeof(struct shared),
                       PROT_READ | PROT_WRITE,
                       MAP_SHARED,
                       fd,
@@ -26,24 +28,22 @@ main(int argc, char *argv[])
 
     close(fd);
 
-    mutex =Sem_open(SEM_NAME,O_CREAT | O_EXCL,FILE_MODE,1);
-    Sem_unlink(SEM_NAME);
+    Sem_init(&ptr->mutex, 1, 1);
 
     setbuf(stdout,NULL);
     if (Fork() == 0) {
         for (i=0; i<nloop; i++) {
-            Sem_wait(mutex);
-            printf("child: %d \n",(*ptr)++);
-            Sem_post(mutex);
+            Sem_wait(&ptr->mutex);
+            printf("child: %d \n",ptr->count++);
+            Sem_post(&ptr->mutex);
         }
         exit(0);
     }
 
     for (i=0; i<nloop; i++) {
-        Sem_wait(mutex);
-        printf("parent: %d \n",(*ptr)++);
-        Sem_post(mutex);
+        Sem_wait(&ptr->mutex);
+        printf("parent: %d \n",ptr->count++);
+        Sem_post(&ptr->mutex);
     }
-
     exit(0);
 }
