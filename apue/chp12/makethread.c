f08a47da96af2877a26833d2841f1581f102c1cf
whj2819 <whj2819@gmail.com>
2013-10-19 23:53:10 +0800

    线程属性

--- apue/chp12/makethread.c
+++ apue/chp12/makethread.c
@@ -18,6 +18,8 @@ makethread(void *(*fn)(void *), void *arg)
     int      err;
     pthread_t    tid;
     pthread_attr_t attr;
+    size_t stacksize;
+    int concurrency;
 
     err = pthread_attr_init(&attr);
     if (err != 0)
@@ -26,16 +28,27 @@ makethread(void *(*fn)(void *), void *arg)
     err = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
     if (err ==0)
         err = pthread_create(&tid, &attr, fn, arg);
+
+
+    err = pthread_attr_getstacksize(&attr, &stacksize);
+    concurrency = pthread_getconcurrency();
     pthread_attr_destroy(&attr);
+
+    printf("stacksize :0x%x concurrency: %d\n", stacksize, concurrency);
     return err;
 }
 
 int main(int argc, const char *argv[])
 {
     int err;
+    int min, max, addr;
 
     err = makethread(&test, NULL);
-    printf("main exit %d ....... \n", err);
+    min = sysconf(_SC_THREAD_ATTR_STACKSIZE);
+    addr = sysconf(_SC_THREAD_ATTR_STACKADDR);
+
+    printf("addr: 0x%x, min: 0x%x  ....... \n", addr, min);
+
 
     sleep(3);
 
