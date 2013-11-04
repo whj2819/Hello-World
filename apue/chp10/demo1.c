3606de634134bf6b7f50e26f4b7c0313f3c6f502
whj2819 <whj2819@gmail.com>
2013-11-04 22:05:41 +0800

    signal function

--- apue/chp10/demo1.c
+++ apue/chp10/demo1.c
@@ -1,1 +1,22 @@
 #include "apue.h"
+
+static void sig_usr(int signo)
+{
+    if (signo == SIGUSR1)
+        printf("received SIGUSR1 \n");
+    else
+        err_dump("received signal %d \n", signo);
+
+}
+
+
+int
+main(void)
+{
+    if (signal(SIGUSR1, sig_usr) == SIG_ERR)
+        err_sys("can't catch SIGUSR1");
+
+   for (; ;)
+       pause();
+
+}
