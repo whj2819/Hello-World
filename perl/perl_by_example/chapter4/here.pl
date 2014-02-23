992b525df5d39c0b3ab2f6244bc43ee6b1432a0d
whj2819 <whj2819@gmail.com>
2014-02-23 10:20:10 +0800

    perl heredoc

--- perl/perl_by_example/chapter4/here.pl
+++ perl/perl_by_example/chapter4/here.pl
@@ -6,7 +6,21 @@ The consumer commented,"As I look over my budget,I'd say
 the price of $price is right.I'll give you \$500 to start."\n
 EOF
 
-print << x 4;
+
+#
+# perl 版本问题
+# 将print << x 4; 改成
+# print <<"" x 4;
+#
+print <<"" x 4;
 Here's to a new day.
 Cheers!
 
+print "\nLet's executs some commands. \n";
+#
+# 这里是反引号
+#
+print <<`END`;
+echo Today is
+date
+END
