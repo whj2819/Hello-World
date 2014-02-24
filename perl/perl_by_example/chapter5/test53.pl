0b91d1a1b4529107b78e6213d94fcde0d353fba3
whj2819 <whj2819@gmail.com>
2014-02-24 11:25:55 +0800

    keys function

---
+++ perl/perl_by_example/chapter5/test53.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+
+
+my %weekday = (
+    1 => 'Monday',
+    2 => 'Tuesday',
+    3 => 'Wednesday',
+    4 => 'Thursday',
+    5 => 'Friday',
+    6 => 'Saturday',
+    7 => 'Sunday'
+);
+
+foreach my $key (keys(%weekday)) {
+    print "$key ";
+}
+print "\n";
+
+foreach my $key (sort keys(%weekday)) {
+    print "$key ";
+}
+print "\n";
