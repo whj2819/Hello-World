adc208345ee470679e0474b6096b0f21a0915f45
whj2819 <whj2819@gmail.com>
2014-03-13 16:57:15 +0800

    mqcreate,open的时候指定属性.

---
+++ unpv22e/chap5/mqcreate.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "unpipc.h"
+
+int main(int argc, char **argv)
+{
+    int c, flags;
+    mqd_t mqd;
+    struct mq_attr attr;
+
+    flags = O_RDWR | O_CREAT;
+    while ( (c = Getopt(argc, argv, "em:z:")) != -1 ) {
+        switch (c) {
+            case 'e':
+                flags |= O_EXCL;
+                break;
+            case 'm':
+                attr.mq_maxmsg = atol(optarg);
+                break;
+            case 'z':
+                attr.mq_msgsize = atol(optarg);
+                break;
+        }
+    }
+    if (optind != argc -1) {
+        err_quit("usage mqcreate [-e] [ -m maxmsg -z msgsize ] <name>");
+    }
+
+    if ( (attr.mq_maxmsg !=0 && attr.mq_msgsize == 0) 
+            || (attr.mq_maxmsg ==0 && attr.mq_msgsize != 0)  ){
+        err_quit("must specify both -m maxmsg and -z msgsize"); 
+    }
+
+    mqd = Mq_open(argv[optind], flags, FILE_MODE, (attr.mq_maxmsg !=0)?&attr:NULL);
+
+    Mq_close(mqd);
+
+    exit(0);
+}
+
