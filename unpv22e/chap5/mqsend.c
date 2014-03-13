4798a040a2505d30e03762cf7ba0abf14d8ea4cb
whj2819 <whj2819@gmail.com>
2014-03-14 00:04:31 +0800

    消息发送和接收

---
+++ unpv22e/chap5/mqsend.c
@@ -0,0 +1,26 @@
+#include "unpipc.h"
+
+typedef unsigned int uint_t;
+
+int
+main(int argc, char *argv[])
+{
+    mqd_t mqd;
+    void *ptr;
+    size_t len;
+    uint_t prio;
+
+    if (argc != 4) {
+        err_quit("usage: mqsend <name> <#bytes> <priority>"); 
+    }
+
+    len = atoi(argv[2]);
+    prio = atoi(argv[3]);
+
+    mqd = Mq_open(argv[1], O_WRONLY);
+    ptr = Calloc(len, sizeof(char));
+
+    Mq_send(mqd, ptr, len, prio);
+
+    exit(0);
+}
