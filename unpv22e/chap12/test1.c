098fe18f4ff02e427cea5bb90188608b956962c5
whj2819 <whj2819@gmail.com>
2014-03-16 21:00:21 +0800

    mmap 测试sigsegv sigbus 信号.

--- unpv22e/chap12/test1.c
+++ unpv22e/chap12/test1.c
@@ -1,5 +1,14 @@
 #include "unpipc.h"
 
+#if 1
+void
+handle(int arg)
+{
+    printf("got SIGSEGV \n");
+    exit (0);
+
+}
+#endif
 
 int
 main(int argc, char **argv)
@@ -18,6 +27,7 @@ main(int argc, char **argv)
     Lseek(fd, filesize-1, SEEK_SET);
     Write(fd, "", 1);
 
+
     ptr = Mmap(
                 NULL
                 , mmapsize
@@ -30,7 +40,7 @@ main(int argc, char **argv)
 
     pagesize = Sysconf(_SC_PAGESIZE);
     printf("PAGESIZE = %ld \n", (long)pagesize);
-
+#if 1
     for(i=0; i<max(filesize, mmapsize); i += pagesize) {
         printf("ptr[%d] = %d \n", i, ptr[i]);
         ptr[i] = 1;
@@ -38,6 +48,35 @@ main(int argc, char **argv)
         ptr[i + pagesize - 1] = 1;
     }
     printf("ptr[%d] = %d \n", i, ptr[i]);
+#endif
+
+#if 0
+    /*
+     * mmap测试. ./test1 foo 8200 8200
+     * 当读取到第3个8192的时候捕获到SIGSEGV信号. 
+     * 那这样子的话页面大小是否就是按8192来
+     * 计算呢.
+     *
+     * 测试sigbus. ./test1 foo 5000 9000
+     * 这样子可以得到sigbus err. 
+     *
+     *
+     */
+    struct sigaction act;
+    act.sa_flags = 0;
+    act.sa_handler = handle;
+    /* signal(SIGSEGV, handle); */
+    sigaction(SIGSEGV, &act, NULL); 
+
+    pagesize = 8192;
+    for(i=0; ; i += pagesize) {
+        printf("ptr[%d] = %d \n", i, ptr[i]);
+        ptr[i] = 1;
+        printf("ptr[%d] = %d \n", i + pagesize - 1, ptr[i + pagesize - 1]);
+        ptr[i + pagesize - 1] = 1;
+    }
+    printf("ptr[%d] = %d \n", i, ptr[i]);
+#endif
 
     exit (0);
 }
