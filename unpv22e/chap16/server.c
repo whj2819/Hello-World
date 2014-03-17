338c7a4f935adbc3349c495f1ed4160ab3f6af2c
whj2819 <whj2819@gmail.com>
2014-03-17 21:27:16 +0800

    SUN RPC

---
+++ unpv22e/chap16/server.c
@@ -0,0 +1,12 @@
+#include "unpipc.h"
+#include "square.h"
+
+square_out *
+squareproc_1_src(square_in *inp, struct svc_req *rqstp)
+{
+    static square_out out;
+
+    out.res1 = inp->arg1 * inp->arg1;
+
+    return (&out);
+}
